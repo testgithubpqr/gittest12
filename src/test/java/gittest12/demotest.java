58d0cb0d65ea77db6700e349efb8d206f2cdbc5d
Admin <ashwini.rao2105@gmail.com>
2024-02-29 00:14:39 +0530

    master first commit

---
+++ src/test/java/gittest12/demotest.java
@@ -0,0 +1,10 @@
+package gittest12;
+
+public class demotest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("master first commit");
+	}
+
+}
