ad6db4249dc17e7e3320cb8a1ed97d3044398af8
Admin <Admin@DESKTOP-HEJABK1.localdomain>
2024-02-29 00:33:47 +0530

    slave first commit

--- src/test/java/gittest12/demotest.java
+++ src/test/java/gittest12/demotest.java
@@ -5,6 +5,7 @@ public class demotest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("master first commit");
+System.out.println("slave first commit");
 	}
 
 }
